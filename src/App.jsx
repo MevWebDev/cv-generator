9819297c9c44e51ab9d3679b9da8da8a410a6cec
MevWebDev <szymongrysiewicz@gmail.com>
2024-09-27 16:23:29 +0200

    <feat> App component && styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+
+import "./App.css";
+import PersonalDetails from "./PersonalDetails";
+import CV from "./CV";
+import Section from "./Section";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [personal, setPersonal] = useState({
+    fullName: "Your Name",
+    email: "yourmail@example.com",
+    phone: "+1 123456789",
+    address: "The wolf street",
+  });
 
+  function handleType(e) {
+    const { name, value } = e.target;
+    console.log(name, value);
+    setPersonal((prevState) => ({ ...prevState, [name]: value }));
+  }
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container">
+        <PersonalDetails handleType={handleType}></PersonalDetails>
+        <Section>Education</Section>
+        <Section>Experience</Section>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <CV personal={personal}></CV>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
